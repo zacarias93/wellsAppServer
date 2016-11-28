10413afa271bb28daed0b630d12de3cb9026ea72
Zac <zeelee93@gmail.com>
2016-11-28 11:27:02 -0500

    h2

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-# INSERT INTO timecard(ID,timeIn,timeOut) VALUES(1,"9:00AM","9:00PM");
-# INSERT INTO timecard(ID,timeIn,timeOut) VALUES(2,"9:00AM","9:00PM");
-# INSERT INTO timecard(ID,timeIn,timeOut) VALUES(3,"9:00AM","9:00PM");
-# INSERT INTO timecard(ID,timeIn,timeOut) VALUES(4,"9:00AM","9:00PM");
