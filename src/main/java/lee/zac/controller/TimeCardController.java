71a471d52580e4016f5e334ef52fbaba6db5b808
Zac <zeelee93@gmail.com>
2016-11-30 16:44:24 -0500

    add tcs

--- src/main/java/lee/zac/controller/TimeCardController.java
+++ src/main/java/lee/zac/controller/TimeCardController.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /*** Created by zaclee on 10/26/16. ***/
 @RestController
@@ -23,16 +25,28 @@ public class TimeCardController {
         return timeCardDAO.findAll();
     }
 
+    @RequestMapping(value = "/timecard/{employeeid}", method = RequestMethod.GET)
+    public List<TimeCard> getEmployeeTimeCards(@PathVariable Long employeeid){
+        Iterable<TimeCard> allTimeCards = timeCardDAO.findAll();
+        List<TimeCard> employeeTimeCards = new ArrayList<>();
+        for(TimeCard tc : allTimeCards) {
+            if(tc.getEmployeeID() == employeeid) {
+                employeeTimeCards.add(tc);
+            }
+        }
+        return employeeTimeCards;
+    }
+
     @RequestMapping(value = "/timecard" , method = RequestMethod.POST)
-    public TimeCard submitTimeCard(@RequestBody TimeCard timeCard){
+    public TimeCard addTimeCard(@RequestBody TimeCard timeCard){
         timeCardDAO.save(timeCard);
         return timeCard;
     }
 
     @RequestMapping(value = "/remove/{id}" , method = RequestMethod.GET)
-    public Iterable<TimeCard> remove(@PathVariable long id) {
+    public String remove(@PathVariable Long id) {
         timeCardDAO.delete(id);
-        return timeCardDAO.findAll();
+        return "{\"message\":\"Timecard removed!\"}";
     }
 
     @RequestMapping(value = "/timecard" , method = RequestMethod.OPTIONS)
--- src/main/java/lee/zac/repository/TimeCardDAO.java
+++ src/main/java/lee/zac/repository/TimeCardDAO.java
@@ -1,11 +1,16 @@
 package lee.zac.repository;
 
 import lee.zac.model.TimeCard;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /*** Created by zaclee on 10/26/16. ***/
 
 @Repository
-public interface TimeCardDAO extends CrudRepository<TimeCard, Long> {
+public interface TimeCardDAO extends JpaRepository<TimeCard, Long> {
+
+    List<TimeCard> findById(@Param("id") Long id);
 }
